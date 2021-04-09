d6b40bb7fa7a7d5f9bc6fa53d92c9e49ba31ed96
seongbinko <wnrhd1082@gmail.com>
2021-04-09 14:07:32 +0900

    init commit

---
+++ src/main/java/com/gitlog/GitlogApplication.java
@@ -0,0 +1,13 @@
+package com.gitlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitlogApplication.class, args);
+    }
+
+}
